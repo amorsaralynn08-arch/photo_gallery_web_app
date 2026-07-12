5e71be5380b96377bedc1d0171f5622cfb2bac03
amor <amorsaralynn@gmail.com>
2026-07-13 01:15:04 +0300

    create superuser and configure django-admin

--- photo_gallery_app/admin.py
+++ photo_gallery_app/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from .models import Profile, Tag, Photo
 
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "created_at",
+        "updated_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+    )
+
+    search_fields = (
+        "name",
+    )
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "user",
+        "total_likes",
+        "total_dislikes",
+        "created_at",
+    )
+
+    list_filter = (
+        "tags",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+        "user__username",
+    )
+
+    filter_horizontal = (
+        "tags",
+        "likes",
+        "dislikes",
+    )
